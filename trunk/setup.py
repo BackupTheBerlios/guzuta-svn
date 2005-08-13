aba5b40b08da3bcb4e0e3289fb496e992ad9fed7
trankas <trankas@d589e6ed-99fd-0310-8b77-8622edddd1f6>
2005-08-13 21:36:46 +0000

    Setup scripts work.

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,6 +6,10 @@ from distutils.core import setup
 
 setup(name='guzuta',
     version='0.0.1',
-    package_dir={'guzuta': 'Guzuta'},
-    packages=['Guzuta']
+    package_dir={'': 'Guzuta'},
+    packages=['guzuta'],
+    data_files=[
+                ('/usr/share/guzuta', ['guzuta2.glade']),
+                ('/usr/bin'
+      ]
     )
