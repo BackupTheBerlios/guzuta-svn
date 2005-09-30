d1624c2098923e7e32c370907634d42414319939
trankas <trankas@d589e6ed-99fd-0310-8b77-8622edddd1f6>
2005-09-30 17:53:27 +0000

    trayicon

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,8 +6,7 @@ setup(name='guzuta',
       version='0.0.1',
       package_dir={'': 'src'},
       packages=['libguzuta'],
-      data_files=[
-                  ('/usr/share/guzuta/', ['data/guzuta3.glade']),
+      data_files=[('/usr/share/guzuta/', ['data/guzuta3.glade']),
                   ('/usr/share/guzuta/', ['data/guzuta_icon_transparent.png']),
                   ('/usr/bin', ['src/guzuta'])],
       )
